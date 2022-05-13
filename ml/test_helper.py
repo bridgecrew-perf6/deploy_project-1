661a6b0c9beb17ced445e8761452d3b70f75e7c8
dmic95 <bakzi95@gmail.com>
2022-05-13 12:13:47 +0200

    All test functions for helper.py

--- ml/test_helper.py
+++ ml/test_helper.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import pytest
-from ml.helper import process_data, get_categorical_features
+from ml.helper import process_data, get_categorical_features, inference
 from joblib import load
 
 @pytest.fixture
@@ -32,6 +32,105 @@ def test_process_data(data):
 
 def test_process_encoder(data):
     """
-    Check 
+    Check encoder structure
     """
     encoder_test = load("data/model/encoder.joblib")
+    lb_test = load("data/model/lb.joblib")
+
+    _, _, encoder, lb = process_data(
+        data, 
+        categorical_features=get_categorical_features(),
+        label="salary",
+        training=True
+    )
+
+    assert encoder.get_params() == encoder_test.get_params()
+    assert lb.get_params() == lb_test.get_params()
+
+def test_inference_above():
+    """
+    Check inference performance
+    """
+
+    model = load("data/model/model.joblib")
+    encoder = load("data/model/encoder.joblib")
+    lb = load("data/model/joblib")
+
+    array = np.array([[
+                35,
+                "Private",
+                "Some-college",
+                "Married-civ-spouse",
+                "Exec-managerial",
+                "Husband",
+                "Black",
+                "Male",
+                80,
+                "United-States"
+            ]])
+
+    df_temp = pd.DataFrame(data=array, columns=[
+        "age",
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "hours-per-week",
+        "native-country",
+    ])
+
+    X, _, _, _ = process_data(
+        df_temp,
+        categorical_features=get_categorical_features(),
+        encoder=encoder,
+        lb=lb,
+        training=False
+    )
+
+    pred = inference(model, X)
+    y = lb.inverse_transform(pred)[0]
+    assert y == ">50k"
+
+def test_inference_below():
+    """
+    Check inference performance
+    """
+    model = load("data/model/model.joblib")
+    encoder = load("data/model/encoder.joblib")
+    lb = load("data/model/lb.joblib")
+
+    array = np.array([[
+                     19,
+                     "Private",
+                     "HS-grad",
+                     "Never-married",
+                     "Own-child",
+                     "Husband",
+                     "Black",
+                     "Male",
+                     30,
+                     "United-States"
+                     ]])
+    df_temp = pd.DataFrame(data=array, columns=[
+        "age",
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "hours-per-week",
+        "native-country",
+    ])
+
+    X, _, _, _ = process_data(
+                df_temp,
+                categorical_features=get_categorical_features(),
+                encoder=encoder, lb=lb, training=False)
+    pred = inference(model, X)
+    y = lb.inverse_transform(pred)[0]
+    assert y == "<=50K"
