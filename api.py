6ca7893aadcabf9b6da27c34d25731202b8b0417
dmic95 <bakzi95@gmail.com>
2022-05-30 14:36:55 +0200

    APT File

--- api.py
+++ api.py
@@ -48,6 +48,13 @@ class User(BaseModel):
         'Holand-Netherlands']
 
 
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
+
+
 app = FastAPI()
 
 @app.get("/")
