bd95ae3f05187ff36381bbe540f8ea29ca0fc13d
dmic95 <bakzi95@gmail.com>
2022-05-30 21:40:55 +0200

    flake8 changes

--- test_herokuapp.py
+++ test_herokuapp.py
@@ -2,25 +2,23 @@
 Heroku Api test script
 """
 import requests
-import pytest
 
 
-@pytest.fixture
-def test_herokuapp():
-    data = {
-        "age": 32,
-        "workclass": "Private",
-        "education": "Some-college",
-        "maritalStatus": "Married-civ-spouse",
-        "occupation": "Exec-managerial",
-        "relationship": "Husband",
-        "race": "Black",
-        "sex": "Male",
-        "hoursPerWeek": 60,
-        "nativeCountry": "United-States"
-    }
+data = {
+    "age": 32,
+    "workclass": "Private",
+    "education": "Some-college",
+    "maritalStatus": "Married-civ-spouse",
+    "occupation": "Exec-managerial",
+    "relationship": "Husband",
+    "race": "Black",
+    "sex": "Male",
+    "hoursPerWeek": 60,
+    "nativeCountry": "United-States"
+}
+r = requests.post('https://udacity-deploy.herokuapp.com/', json=data)
 
-    r = requests.post('https://udacity-deploy.herokuapp.com/', json=data)
+assert r.status_code == 200
 
-    assert r.status_code == 200
-    assert r.json() == {"prediction": ">50K"}
+print("Response code: %s" % r.status_code)
+print("Response body: %s" % r.json())
